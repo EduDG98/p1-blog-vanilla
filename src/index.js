796e753cc59fe2cbda845bf7a9ea9673a637fb2c
Edu <edu_marzo_98@hotmail.com>
2025-02-08 13:49:49 +0000

    Configurate search bar to search clothe

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-import './components/ClotheComponent'
+const searchBar = document.querySelector('#search-bar')
+const clotheElement = document.querySelectorAll('.clothe-name')
+const clotheContainer = document.getElementsByClassName('clothe-container')
 
+const detectInput = e => {
+    const value = e.target.value.toLowerCase()
+    
+    clotheElement.forEach((clothe, index) => {
+        if(clothe.textContent.toLocaleLowerCase().includes(value)){
+            clotheContainer[index].classList.remove('hide')
+        }else{
+            clotheContainer[index].classList.add('hide')
+    }
+})
+}
+
+searchBar.addEventListener('input', detectInput)
